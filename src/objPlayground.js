eb84f077cc5e3e3ec9d68ea8681912e7843e6ec5
Felipe <felipeicloud11@gmail.com>
2023-02-28 16:28:17 -0300

    cumprindo sétimo requisito

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -34,8 +34,20 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => {
+  const object = {
+    sum: number1 + number2,
+    mult: number1 * number2,
+    div: Math.floor(number1 / number2),
+    sub: number1 - number2,
+  };
 
-const arrayGenerator = (type, object) => {};
+  return object;
+};
+
+const arrayGenerator = (type, object) => {
+  const array = Object[`${type}`](object);
+  return array;
+};
 
 module.exports = { calculator, arrayGenerator };
