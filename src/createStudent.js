fc94ae23a3a23df3c96838bf51bd943a08bd9304
Felipe <felipeicloud11@gmail.com>
2023-02-28 16:01:25 -0300

    cumprindo quinto requisito

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (guivenName) => {
+  const object = {
+    name: guivenName,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return object;
+};
 
 module.exports = createStudent;
